ea612d632f8816048c2c38c411dec5a3cec98190
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-08-08 08:18:43 +0530

    first commit

---
+++ AddressBook.java
@@ -0,0 +1,92 @@
+package com.Addressbook;
+import java.util.*;
+
+public class AddressBook {
+
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String state;
+	private int zip; 
+	private long phoneNumber;   
+	private String email;
+	
+	public AddressBook() {
+		this.firstName=null;
+		this.lastName=null;
+		this.city=null;
+		this.state=null;;
+		this.zip=0;
+		this.phoneNumber=0;   
+		this.email=null;
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBook [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + ", state=" + state
+				+ ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+
+
+
+	
+
+
+	}
---
+++ Contact.java
@@ -0,0 +1,102 @@
+package com.Addressbook;
+import java.util.*;
+
+
+public class Contact {
+
+public static void main(String[] args) {
+        	
+        	List<AddressBook> person = new ArrayList<AddressBook>();
+        	Scanner sc = new Scanner(System.in);
+        	while(true) {
+        		System.out.println("Welcome To Address Book System\n\n"
+        				+ "Choose your option:\n"
+        				+ "1. Create new contact\n"
+        				+ "2. View all the existing contacts\n"
+        				+ "3. Edit existing contact\n"
+        				+ "4. Remove The contact\n"
+        				+ "5. Quit\n");
+        		int option = sc.nextInt();
+                sc.nextLine();
+                switch(option){
+                //Add New Contact
+                    case 1:
+                    	AddressBook address = new AddressBook();
+                    	
+                    	System.out.print("Enter Your First Name: ");
+                    	address.setFirstName(sc.nextLine());
+            			
+            			System.out.print("Enter Your Last Name: ");
+            			address.setLastName(sc.nextLine());
+            			
+            			System.out.print("Enter Your address: ");            		
+            			address.setCity(sc.nextLine());
+
+            			System.out.print("Enter Your City: ");            		
+            			address.setCity(sc.nextLine());
+            			
+            			System.out.print("Enter Your State Name: ");         		
+            			address.setState(sc.nextLine());
+
+            			
+            			System.out.print("Enter Your Zip Code: ");           			
+            			address.setZip(sc.nextInt());
+
+            			
+            			System.out.print("Enter Your Phone Number: ");
+            			address.setPhoneNumber(sc.nextLong());
+
+            			
+            			System.out.print("Enter Your Email Id: ");
+            			address.setEmail(sc.next());
+            			
+            			 person.add(address);
+            			 break;
+            			 //View all existing contact
+                    case 2:
+                        
+                        for (AddressBook addPerson : person) {
+                            System.out.println(addPerson);
+                        }
+                        System.out.println("\n\n");
+                        break;
+                       // edit existing contact
+                    case 3:
+                    	System.out.println("Edit record");
+                    	System.out.println("Enter name to edit:");
+                    	for(int i=0;i<person.size();i++){
+               			address=person.get(i);
+                         	
+                      if(sc.nextLine().equals(address.getFirstName())) {
+                    		 System.out.println("Enter new city:");
+            				 address.setCity(sc.nextLine());    				 
+            				 System.out.println("List After is"+person.toString());
+                      }
+            				 else {
+            					 System.out.println("User not found");
+            				 }
+                    	}
+            				 break;
+            		//Remove contact
+                    case 4:
+                    	System.out.println("remove record");
+                    	System.out.println("Enter name to remove:");
+                    	for(int i=0;i<person.size();i++){
+               			address=person.get(i);
+               			if(sc.nextLine().equals(address.getFirstName())) {
+               			person.remove(address);
+               			System.out.println("List After removing"+person.toString());
+               			}
+               			else {
+              				 System.out.println("User not found");
+              			 }
+                    	}
+               				 break;
+               			 
+               		
+                    case 5:
+                        return;
+            	}
+            	}
+    }
+    }
